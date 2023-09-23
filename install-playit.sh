6c896adf31f6c32473e6b49c5fa7ee74ed3437b8
catacastrophy <140408893+catacastrophy@users.noreply.github.com>
2023-09-23 15:12:24 +0200

    Playit installer v1.1

--- install-playit.sh
+++ install-playit.sh
@@ -1,7 +1,14 @@
 echo "Welcome to playit.gg installer | UNOFFICIAL"
-sudo apt install curl # Making sure CURL is installed
-sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
+echo "Running: sudo apt update"
 sudo apt update
+echo "Adding PPA key"
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
+echo "Adding playit to apt list"
+sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
+echo "Updating"
+sudo apt update
+echo "Installing playit"
 sudo apt install playit
-echo "Playit has been installed, running..."
-playit
+echo "Playit has been installed!"
+chmod 755 ./background-playit.sh
+./background-playit.sh
